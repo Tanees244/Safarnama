826294415059912cd1e4fb5488d4ef7d814b4c5c
Tanees <k201879@nu.edu.pk>
2023-10-26 02:46:59 +0500

    tan

--- Screens/Airline/AirlineDashboard.js
+++ Screens/Airline/AirlineDashboard.js
@@ -1,47 +1,64 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, TextInput, ScrollView, Image, TouchableOpacity, Dimensions } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import Modal from 'react-native-modal';
+import DropDownPicker from 'react-native-dropdown-picker';
 import { useNavigation } from '@react-navigation/native';
 
 const AirlineDashboard = () => {
 
-  const screenWidth = Dimensions.get('window').width;
-  const containerWidth = screenWidth * 0.9;
-  const RegisterContainer = containerWidth * 0.7;
-  const inputWidth = containerWidth * 0.9;
-  
-  const navigation = useNavigation();
-  
-  const [text, setText] = useState('');
-  const textInputRef = useRef(null);
-  const [image, setImage] = useState(null);
+    const screenWidth = Dimensions.get('window').width;
+    const containerWidth = screenWidth * 0.9;
+    const RegisterContainer = containerWidth * 0.7;
+    const inputWidth = containerWidth * 0.9;
+    
+    const navigation = useNavigation();
+    
+    const [text, setText] = useState('');
+    const textInputRef = useRef(null);
+    const [isAddTicketModalVisible, setAddTicketModalVisible] = useState(false);
+    const [fromCity, setFromCity] = useState('');
+    const [toCity, setToCity] = useState('');
+    const [price, setPrice] = useState('');
+    const [seatType, setSeatType] = useState(null); // Initialize with null
+    const seatTypes = [
+    { label: 'Economy', value: 'economy' },
+    { label: 'Business', value: 'business' },
+    { label: 'First Class', value: 'first-class' },
+    ]; // Define your list of seat types
 
-  const handleTextChange = (newText) => {
-    setText(newText);
 
-    const totalHeight = (newText.split('\n').length * 25) + 50;
+    // Function to show/hide the modal
+    const toggleAddTicketModal = () => {
+    setAddTicketModalVisible(!isAddTicketModalVisible);
+    };
 
-    if (textInputRef.current) {
-      textInputRef.current.setNativeProps({
-        height: Math.max(55, totalHeight),
-      });
-    }
-  };
+    const handleTextChange = (newText) => {
+        setText(newText);
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+        const totalHeight = (newText.split('\n').length * 25) + 50;
 
-    console.log(result);
+        if (textInputRef.current) {
+        textInputRef.current.setNativeProps({
+            height: Math.max(55, totalHeight),
+        });
+        }
+    };
 
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
-  };
+    const handleFromCityChange = (value) => {
+        setFromCity(value);
+    };
+    
+    const handleToCityChange = (value) => {
+        setToCity(value);
+    };
+    
+    const handlePriceChange = (value) => {
+        setPrice(value);
+    };
+    
+    const handleSeatTypeChange = (value) => {
+        setSeatType(value);
+    };
   
 
   return (
@@ -56,12 +73,51 @@ const AirlineDashboard = () => {
             Welcome <Text style={[styles.Text, { color: 'white' }]}>Vendor</Text>
           </Text>
         </View>
-        <View style={styles.ButtonContainer}>
-          <View style={styles.PackageContainer}>
-            <Text>Hello</Text>
+        <View style={styles.PackageContainer}>
+          <View style={styles.ButtonContainer}>
+            <TouchableOpacity onPress={toggleAddTicketModal} style={styles.AddTicketButton}>
+                <Text style={styles.AddTicketButtonText}>Add Tickets</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
+
+      <Modal isVisible={isAddTicketModalVisible}>
+        <View style={styles.modalContent}>
+            <Text>Add Ticket Form</Text>
+            <TextInput
+            placeholder="From City"
+            value={fromCity}
+            onChangeText={handleFromCityChange}
+            style={styles.input}
+            />
+            <TextInput
+            placeholder="To City"
+            value={toCity}
+            onChangeText={handleToCityChange}
+            style={styles.input}
+            />
+            <TextInput
+            placeholder="Price"
+            value={price}
+            onChangeText={handlePriceChange}
+            keyboardType="numeric"
+            style={styles.input}
+            />
+            <DropDownPicker
+            items={seatTypes}
+            placeholder="Select Seat Type"
+            value={seatType}
+            containerStyle={styles.dropdownContainer}
+            style={styles.dropdown}
+            itemStyle={styles.dropdownItem}
+            onChangeItem={handleSeatTypeChange}
+            />
+            <TouchableOpacity onPress={toggleAddTicketModal}>
+            <Text>Close</Text>
+            </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -72,7 +128,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   Container: {
-    backgroundColor: '#181922',
+    backgroundColor: '#e9eaec',
     flex: 1,
   },
   header: {
@@ -90,8 +146,10 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-Bold',
   },
   RegisterContainer: {
+    backgroundColor: '#272a3e',
     paddingVertical: 20,
     marginTop: 20,
+    borderRadius: 30,
     marginBottom: 10,
     zIndex: 1,
     alignItems: 'center',
@@ -104,15 +162,63 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   ButtonContainer: {
-    marginTop: 20,
     marginHorizontal: 20,
     marginVertical: 20,
     borderRadius: 20,
-    paddingHorizontal: 20,
-    paddingVertical: 20,
   },
   PackageContainer:{
     backgroundColor: '#262a3e',
+    marginTop: 20,
+    borderRadius: 20,
+    width: '90%',
+    height: 200,
+  },
+  AddTicketButton: {
+    backgroundColor: '#54aaec',
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  AddTicketButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontFamily: 'Poppins-Bold',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+  },
+  closeIconContainer: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    backgroundColor: '#C4C8CB',
+    borderRadius: 60,
+  },
+  closeIcon: {
+    width: 25,
+    height: 25,
+  },
+  
+  input: {
+    backgroundColor: '#b6daf7',
+    paddingHorizontal: 15,
+    borderRadius: 15,
+    fontFamily: 'Poppins-Regular',
+    height: 50,
+    marginBottom: 10,
+  },
+  dropdownContainer: {
+    height: 50,
+    borderRadius: 15,
+    marginBottom: 10,
+  },
+  dropdown: {
+    backgroundColor: '#b6daf7',
+  },
+  dropdownItem: {
+    justifyContent: 'flex-start',
   },
 });
 
