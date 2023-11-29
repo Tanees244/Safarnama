671a97aac3fd50f10447b112d8b17760da8de9da
Tanees <k201879@nu.edu.pk>
2023-11-30 01:41:39 +0500

    ok

--- Screens/Tourists/Discover.js
+++ Screens/Tourists/Discover.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert, Image, FlatList, ImageBackground, ScrollView, Dimensions } from 'react-native';
+import React , {useState} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert, Image, FlatList, ImageBackground, ScrollView, Dimensions, TextInput, Animated } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { BlurView } from 'expo-blur';
 
@@ -174,6 +174,42 @@ const Discover = () => {
 
   const navigation = useNavigation();
 
+  const [isExpanded, setIsExpanded] = useState(false);
+    const scaleValue = new Animated.Value(0);
+
+    const navigateToHotesInfo = () => {
+        navigation.navigate('HotelsInfo')
+    }
+
+    const toggleMenu = () => {
+      const toValue = isExpanded ? 0 : 1;
+      Animated.timing(scaleValue, {
+          toValue,
+          duration: 0,
+          useNativeDriver: true,
+      }).start();
+      setIsExpanded(!isExpanded);
+    }
+
+    const handleMenuItemPress = (menuItem) => {
+        // Handle navigation based on the selected menu item
+        // For example:
+        if (menuItem === 'Home') {
+          navigation.navigate('Discover')
+        } else if (menuItem === 'Profile') {
+          navigation.navigate('TouristProfile')
+        } else if (menuItem === 'Booking') {
+          navigation.navigate('CreatePackage')
+        }
+        // Collapse the menu after selection
+        toggleMenu();
+    }
+
+    const scale = scaleValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [1, 4],
+    });
+
   const navigateToHotelsInfo = () =>{
     navigation.navigate('HotelsLists')
   };
@@ -220,13 +256,46 @@ const combinedData = [...data, ...data2];
       <View style={styles.header}>
         <Text style={styles.headerText}>Safarnama</Text>
       </View>
+        {isExpanded && (
+                <View style={styles.expandedMenu}>
+                    <TouchableOpacity
+                        style={styles.expandedMenuItem}
+                        onPress={() => handleMenuItemPress('Home')}
+                    >
+                        <Image source={require("../../assets/Home.png")} style = {[{width: 40, height: 40}]} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.expandedMenuItem}
+                        onPress={() => handleMenuItemPress('Profile')}
+                    >
+                        <Image source={require("../../assets/account-circle-black.png")} style = {[{width: 40, height: 40}]}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.expandedMenuItem}
+                        onPress={() => handleMenuItemPress('Booking')}
+                    >
+                        <Image source={require("../../assets/booking.png")} style = {[{width: 40, height: 40}]}/>
+                    </TouchableOpacity>
+                </View>
+            )}
+            <TouchableOpacity
+                style={styles.HomeButton}
+                onPress={toggleMenu}
+                activeOpacity={0.5}
+            >
+                {isExpanded ? (
+                    // Render close icon or any icon you prefer
+                    <Image source={require("../../assets/WhiteClose.png")} style = {[{width: 30, height: 30}]}/>
+                ) : (
+                    // Your existing Home button content
+                    <Image source={require("../../assets/ViewMore.png")} style = {[{width: 35, height: 35}]}/>
+                )}
+            </TouchableOpacity>
+      
     <ScrollView>
       <View style={styles.quote}>
         <Text style={styles.quotetext}>Let's find your best{'\n'}<Text style={{color: '#c7f3ff'}}>Travel plans ?</Text></Text>
       </View>
-      
-      <Text style={styles.text}>Popular Categories</Text>
-      <View style={[styles.buttonContainer, { width: containerWidth }]}>
 
         {/* searchbar */}
         <TextInput
@@ -251,7 +320,8 @@ const combinedData = [...data, ...data2];
             )}
           />
         )}
-
+      
+      <Text style={styles.text}>Popular Categories</Text>
         <View style={[styles.buttonContainer, { width: containerWidth }]}>
           <TouchableOpacity style={[styles.buttons, {width: buttonWidth}]} onPress={navigateToFlight}>
           <View style={styles.buttonContent}>
@@ -323,7 +393,6 @@ const combinedData = [...data, ...data2];
           renderItem={({ item }) => <HorizontalCard item={item} />}
           keyExtractor={(item) => item.id}
         />
-    </View>
   </ScrollView>
   </View>
   );
@@ -352,12 +421,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   searchInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
+    height: 60,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderWidth: 2,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginHorizontal: 10,
+    fontFamily: 'Poppins-Regular',
   },
   resultHeader: {
     fontSize: 18,
@@ -592,6 +663,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     elevation: 5,
     zIndex: 2,
+  },
+  HomeButton: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5,
+    zIndex: 2,
+  },
+  expandedMenu: {
+      position: 'absolute',
+      bottom: 10,
+      left: 100,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      zIndex: 3,
+  },
+  expandedMenuItem: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+      backgroundColor: 'black',
+      margin: 10,
   },  
 });
 
